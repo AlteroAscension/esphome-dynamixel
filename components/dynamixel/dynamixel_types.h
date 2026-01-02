7b9e1e38c26e34b021c612955074672e241240ef
AlteroAscension <37866011+AlteroAscension@users.noreply.github.com>
2026-01-03 01:55:17 +0300

    Add components/dynamixel/dynamixel_types.h

---
+++ components/dynamixel/dynamixel_types.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace esphome {
+namespace dynamixel {
+
+enum class ProtocolVersion : uint8_t { V1 = 1, V2 = 2 };
+
+}  // namespace dynamixel
+}  // namespace esphome
