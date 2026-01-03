fde043c22e78ce8fc4f0a45aecb6f838843b55da
AlteroAscension <37866011+AlteroAscension@users.noreply.github.com>
2026-01-03 06:23:10 +0300

    Fix register key import

--- components/dynamixel/sensor.py
+++ components/dynamixel/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID, CONF_DEVICE_ID, CONF_REGISTER, CONF_MULTIPLY, CONF_OFFSET
+from esphome.const import CONF_ID, CONF_DEVICE_ID, CONF_MULTIPLY, CONF_OFFSET
 
 from . import dynamixel_ns, DynamixelComponent
 
@@ -15,7 +15,7 @@ CONFIG_SCHEMA = (
         {
             cv.Required("dynamixel_id"): cv.use_id(DynamixelComponent),
             cv.Required(CONF_DEVICE_ID): cv.uint8_t,
-            cv.Required(CONF_REGISTER): cv.string,
+            cv.Required("register"): cv.string,
             cv.Optional(CONF_MULTIPLY, default=1.0): cv.float_,
             cv.Optional(CONF_OFFSET, default=0.0): cv.float_,
             cv.Optional("signed", default=False): cv.boolean,
@@ -33,6 +33,6 @@ async def to_code(config):
     parent = await cg.get_variable(config["dynamixel_id"])
     cg.add(var.set_parent(parent))
     cg.add(var.set_device_id(config[CONF_DEVICE_ID]))
-    cg.add(var.set_register_name(config[CONF_REGISTER]))
+    cg.add(var.set_register_name(config["register"]))
     cg.add(var.set_scale(config[CONF_MULTIPLY], config[CONF_OFFSET]))
     cg.add(var.set_signed(config["signed"]))
