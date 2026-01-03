2bc95112fff125be3cf354d22ae337a39cf4f610
AlteroAscension <37866011+AlteroAscension@users.noreply.github.com>
2026-01-03 06:25:42 +0300

    Rename device_id key for entities

--- components/dynamixel/sensor.py
+++ components/dynamixel/sensor.py
@@ -1,9 +1,11 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID, CONF_DEVICE_ID, CONF_MULTIPLY, CONF_OFFSET
+from esphome.const import CONF_ID, CONF_MULTIPLY, CONF_OFFSET
 
 from . import dynamixel_ns, DynamixelComponent
+
+CONF_DXL_DEVICE_ID = "dxl_device_id"
 
 DEPENDENCIES = ["dynamixel"]
 
@@ -14,7 +16,7 @@ CONFIG_SCHEMA = (
     .extend(
         {
             cv.Required("dynamixel_id"): cv.use_id(DynamixelComponent),
-            cv.Required(CONF_DEVICE_ID): cv.uint8_t,
+            cv.Required(CONF_DXL_DEVICE_ID): cv.uint8_t,
             cv.Required("register"): cv.string,
             cv.Optional(CONF_MULTIPLY, default=1.0): cv.float_,
             cv.Optional(CONF_OFFSET, default=0.0): cv.float_,
@@ -32,7 +34,7 @@ async def to_code(config):
 
     parent = await cg.get_variable(config["dynamixel_id"])
     cg.add(var.set_parent(parent))
-    cg.add(var.set_device_id(config[CONF_DEVICE_ID]))
+    cg.add(var.set_device_id(config[CONF_DXL_DEVICE_ID]))
     cg.add(var.set_register_name(config["register"]))
     cg.add(var.set_scale(config[CONF_MULTIPLY], config[CONF_OFFSET]))
     cg.add(var.set_signed(config["signed"]))
