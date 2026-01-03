b8bf322c8f0dd357e4ffa6d42ff4c41a0170c40b
AlteroAscension <37866011+AlteroAscension@users.noreply.github.com>
2026-01-03 06:25:41 +0300

    Rename device_id key for entities

--- components/dynamixel/switch.py
+++ components/dynamixel/switch.py
@@ -1,9 +1,11 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
-from esphome.const import CONF_ID, CONF_DEVICE_ID
+from esphome.const import CONF_ID
 
 from . import dynamixel_ns, DynamixelComponent
+
+CONF_DXL_DEVICE_ID = "dxl_device_id"
 
 DEPENDENCIES = ["dynamixel"]
 
@@ -12,7 +14,7 @@ DynamixelSwitch = dynamixel_ns.class_("DynamixelSwitch", switch.Switch)
 CONFIG_SCHEMA = switch.switch_schema(DynamixelSwitch).extend(
     {
         cv.Required("dynamixel_id"): cv.use_id(DynamixelComponent),
-        cv.Required(CONF_DEVICE_ID): cv.uint8_t,
+        cv.Required(CONF_DXL_DEVICE_ID): cv.uint8_t,
         cv.Required("register"): cv.string,
     }
 )
@@ -24,5 +26,5 @@ async def to_code(config):
 
     parent = await cg.get_variable(config["dynamixel_id"])
     cg.add(var.set_parent(parent))
-    cg.add(var.set_device_id(config[CONF_DEVICE_ID]))
+    cg.add(var.set_device_id(config[CONF_DXL_DEVICE_ID]))
     cg.add(var.set_register_name(config["register"]))
