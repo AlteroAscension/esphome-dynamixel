451cd998acaa52f7d50ffcb7b47d8e18492574a1
AlteroAscension <37866011+AlteroAscension@users.noreply.github.com>
2026-01-03 05:33:07 +0300

    Fix UART register call

--- components/dynamixel/__init__.py
+++ components/dynamixel/__init__.py
@@ -52,7 +52,7 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await uart.register_uart_device(var, config[CONF_UART_ID])
+    await uart.register_uart_device(var, config)
 
     cg.add(var.set_default_protocol(config[CONF_PROTOCOL]))
 
